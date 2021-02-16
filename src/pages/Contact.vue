dfdb58b15e3fc11334119a149d0984e120e1cacd
Raphael Sanchez <r.sanchez@normandiewebschool.fr>
2021-02-16 09:15:48 +0100

    Update contact page infos

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -3,20 +3,18 @@
     <header class="text-center">
       <h1>Rendez-vous</h1>
       <p class="lead">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
-        doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur
-        rerum illum odit fugit assumenda rem dolores inventore iste
-        reprehenderit maxime! Iusto.
+        Pour plus d’informations, n’hésitez pas à nous contacter pour prendre rendez-vous. Nous vous recevrons au showroom avec plaisir.
       </p>
-      <a href="tel:+33235023502">02 35 02 35 02</a>
-      <br>
-      <a href="mailto:contact@lanef-passion.fr">contact@lanef-passion.fr</a>
     </header>
 
     <section class="text-center">
-      <h2>Horaires</h2>
+      <h2>Nous Contacter</h2>
+      <a href="tel:+33235023502">02 35 02 35 02</a>
+      <br>
+      <a href="mailto:contact@lanef-passion.fr">contact@lanef-passion.fr</a>
+      <h2>Passez nous voir</h2>
       <p><a href="https://goo.gl/maps/iFQSSxNjXYx2Zgpi9" target="_blank">12 bis avenue Carnot 76250 Déville-lès-Rouen</a></p>
-      <p>Du lundi au samedi de 9h00 à 18h00</p>
+      <p>Du lundi au samedi de 9h30 à 12h30 et de 14h à 18h</p>
     </section>
   </Layout>
 </template>
