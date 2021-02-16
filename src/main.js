57743340bd2714cb03cc70d0723a890bed390153
Raphael Sanchez <r.sanchez@normandiewebschool.fr>
2021-02-16 09:12:51 +0100

    Import global styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
+// Import Global styles
+import '~/assets/styles.css'
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
